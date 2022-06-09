1a4d34b8ff8136ad205ffb03f0f96168c5f9adbf
Aditya Sriram <aweditya@gmail.com>
2022-06-09 22:22:05 +0800

    Added code to vary N, M by passing command-line arguments

--- gaussian-elimination/gaussian-elimination-serial.c
+++ gaussian-elimination/gaussian-elimination-serial.c
@@ -2,16 +2,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define N 4
-#define M 2
+#include "omp.h"
 
 double drand(double low, double high)
 {
     return ((double)rand() * (high - low)) / (double)RAND_MAX + low;
 }
 
-void init_matrix(double *a)
+void init_matrix(double *a, int N, int M)
 {
     for (int i = 0; i < N; i++)
     {
@@ -19,13 +17,13 @@ void init_matrix(double *a)
         {
             for (int k = 0; k <= M; k++)
             {
-                a[i * M * (M + 1) + j * (M + 1) + k] = drand(1.0, 10.0);
+                a[i * M * (M + 1) + j * (M + 1) + k] = drand(-1.0, 1.0);
             }
         }
     }
 }
 
-void print_matrix(double *a)
+void print_matrix(double *a, int N, int M)
 {
     for (int i = 0; i < N; i++)
     {
@@ -48,7 +46,7 @@ void swap(double *a, double *b)
     *b = temp;
 }
 
-void solve_one_instance(double *instance)
+void solve_one_instance(double *instance, int N, int M)
 {
     int i, j, k = 0, c, flag = 0, m = 0;
 
@@ -91,16 +89,16 @@ void solve_one_instance(double *instance)
         instance[i * (M + 1) + M] /= instance[i * (M + 2)];
 }
 
-void solve_serial(double *a)
+void solve_serial(double *a, int N, int M)
 {
     for (int i = 0; i < N; i++)
     {
-        solve_one_instance(a + i * M * (M + 1));
+        solve_one_instance(a + i * M * (M + 1), N, M);
     }
 }
 
 // L2 norm of solution (last column of instance)
-double l2_norm(double *instance)
+double l2_norm(double *instance, int N, int M)
 {
     double norm = 0.0;
     for (int i = 0; i < M; i++)
@@ -111,28 +109,36 @@ double l2_norm(double *instance)
     return norm;
 }
 
-double sum_l2(double *a)
+double sum_l2(double *a, int N, int M)
 {
     double sum = 0.0;
     for (int i = 0; i < N; i++)
     {
-        sum += l2_norm(a + i * M * (M + 1));
+        sum += l2_norm(a + i * M * (M + 1), N, M);
     }
 
     return sum;
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    int N = 4, M = 2;
+
+    if (argc == 3)
+    {
+        N = atoi(argv[1]);
+        M = atoi(argv[2]);
+    }
+
     double *a;
     a = malloc(N * M * (M + 1) * sizeof(double));
 
     // Random Initialisation
-    init_matrix(a);
+    init_matrix(a, N, M);
  
     // Solve using GEM (serial)
-    solve_serial(a);
-    printf("Sum of L2 norm: %lf\n", sum_l2(a));
+    solve_serial(a, N, M);
+    printf("Sum of L2 norm: %lf\n", sum_l2(a, N, M));
     
     return 0;
 }
