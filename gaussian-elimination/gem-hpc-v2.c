a3885cc14ccb2592627b834d719a2b5d904e748a
Aditya Sriram <aweditya@gmail.com>
2022-06-26 22:15:23 +0800

    Some optimizations

--- gaussian-elimination/gem-hpc-v2.c
+++ gaussian-elimination/gem-hpc-v2.c
@@ -6,6 +6,11 @@
 #include "sched.h"
 #include "omp.h"
 
+double drand(double low, double high, unsigned int *seed)
+{
+    return ((double)rand_r(seed) * (high - low)) / (double)RAND_MAX + low;
+}
+
 void init_vars(int *N, int *M, int *mode)
 {
     const char *number_of_instances = getenv("N");
@@ -24,6 +29,18 @@ void init_vars(int *N, int *M, int *mode)
     if (running_mode)
     {
         *mode = atoi(running_mode);
+    }
+}
+
+void init_matrix(double *instance, unsigned int M)
+{
+    unsigned int seed = 25234 + 17*omp_get_thread_num();
+    for (int row = 0; row < M; row++)
+    {
+        for (int column = 0; column <= M; column++)
+        {
+            instance[row * (M + 1) + column] = drand(-1.0, 1.0, &seed);
+        }
     }
 }
 
@@ -52,27 +69,22 @@ void initialize_and_solve(int M)
     double *instance;
     instance = malloc(M * (M + 1) * sizeof(double));
 
-    // Initialise the matrix
-    unsigned int seed = 25234 + 17*omp_get_thread_num();
-    for (int row = 0; row < M; row++)
-    {
-        for (int column = 0; column <= M; column++)
-        {
-            instance[row * (M + 1) + column] = ((double)rand_r(&seed) * 2.0) / (double)RAND_MAX - 1.0;
-        }
-    }
-
-    // print_matrix(instance, M);
-
-    // Performing elementary operations
     int i, j, k = 0, c, flag = 0, m = 0;
+    
+    // Initialise the matrix
+    init_matrix(instance, M);
+    
+    // Performing elementary operations
     for (i = 0; i < M; i++)
     {
         if (instance[i * (M + 2)] == 0)
         {
             c = 1;
-            while ((i + c) < M && instance[(i + c) * (M + 1) + i] == 0)
+            int idx = (i + c) * (M + 1) + i;
+            while ((i + c) < M && instance[idx] == 0) {
                 c++;
+                idx = (i + c) * (M + 1) + i;
+            }
 
             if ((i + c) == M)
             {
@@ -80,10 +92,12 @@ void initialize_and_solve(int M)
                 break;
             }
 
-            for (j = i, k = 0; k <= M; k++)
-            {
-                swap(&instance[j * (M + 1) + k], &instance[(j + c) * (M + 1) + k]);
+            for (j = i, k = 0; k <= M; k++) {
+                int lhs = j * (M + 1) + k;
+                int rhs = (j + c) * (M + 1) + k;
+                swap(&instance[lhs], &instance[rhs]);
             }
+
         }
 
         for (j = 0; j < M; j++)
@@ -93,10 +107,15 @@ void initialize_and_solve(int M)
             {
                 // Converting Matrix to reduced row
                 // echelon form(diagonal matrix)
-                double pro = instance[j * (M + 1) + i] / instance[i * (M + 2)];
+                int idx_num = j * (M + 1) + i;
+                int idx_den = i * (M + 2);
+                double pro = instance[idx_num] / instance[idx_den];
 
-                for (k = 0; k <= M; k++)
-                    instance[j * (M + 1) + k] -= (instance[i * (M + 1) + k]) * pro;
+                for (k = 0; k <= M; k++) {
+                    int idx1 = j * (M + 1) + k;
+                    int idx2 = i * (M + 1) + k;
+                    instance[idx1] -= (instance[idx2]) * pro;
+                }
             }
         }
     }
@@ -107,10 +126,7 @@ void initialize_and_solve(int M)
         instance[i * (M + 1) + M] /= instance[i * (M + 2)];
     }
 
-    // print_matrix(instance, M);
-
-    free(instance);
-    instance = NULL;
+    free(instance); instance = NULL;
 }
 
 double solve_serial(int N, int M)
