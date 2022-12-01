cade16c170d0a1675d461fbead3775c559150a6c
Aditya <aweditya@gmail.com>
2022-12-01 14:10:39 +0530

    first fork-exec model test

---
+++ pso/ngspice-integration/ipc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+	char* argv[] = {"Command-line", "test.cir", NULL};
+	int pid = fork();
+	if ( pid == 0)
+	{
+		printf("Inside child process\n");
+		printf("Process ID (pid) of child is: %d\n", (int)getpid());
+		execvp("ngspice", argv);
+	}
+	else
+	{
+		printf("Inside parent process - pid of child is %d\n", pid);
+		printf("Process ID (pid) of parent is: %d\n", (int)getpid());
+
+		printf("%ld\n", strlen(NULL));
+	}
+	return 0;
+}
